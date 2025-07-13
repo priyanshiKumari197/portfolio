cfba840c82b8a475fa8663abffdaabb55810a600
priyanshiKumari197 <priyanshik674@gamil.com>
2025-07-13 18:33:21 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuList = document.querySelector("#menu-list");
+let menuBar = document.querySelector("#menu-bar")
+
+function toggleMenubar(){
+    if(menuList.style.maxHeight === "0px" || menuList.style.maxHeight === ""){
+        menuList.style.maxHeight = "300px";
+    } else {
+        menuList.style.maxHeight = "0px";
+    }
+    console.log("click button");
+}
+clearInterval
